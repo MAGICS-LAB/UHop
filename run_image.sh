cdac75431df968b7142b4fb605a0fcd56feb59cb
Dennis Wu <67745938+Hibb-bb@users.noreply.github.com>
2024-06-03 17:17:45 -0500

    Update run_image.sh

--- run_image.sh
+++ run_image.sh
@@ -6,7 +6,7 @@ done
 
 for datasize in 1000 2000 5000 10000 20000 50000
 do  
-    python3 image_classification.py --data cifar10 --datasize $datasize     CUDA_VSIBLE_DEVICES=2 python3 deep_ViH.py --data tiny_imagenet --datasize 60000 --n_class 200 --init_lr 0.0001 --batch_size 1024
+    python3 image_classification.py --data cifar10 --datasize $datasize
 
 done
 
